aa50f9a91089c4626000913829d55aba4c468253
shrey <spatel3546@gmail.com>
2024-05-30 11:00:26 +0530

    navitems done

--- eveman/components/shared/Header.tsx
+++ eveman/components/shared/Header.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from '../ui/button'
+import NavItems from './NavItems'
+import MobileNav from './MobileNav'
+
 
 const Header = () => {
   return (
@@ -14,9 +17,18 @@ const Header = () => {
                 alt ="Eveman Logo"
                 />
             </Link>
+
+            <SignedIn>
+                <nav className="md:flex-between hidden w-full max-w-xs ">
+                    <NavItems />
+                </nav>
+            </SignedIn> 
+
             <div className='flex w-32 justify-end gap-3'>
                 <SignedIn>
                     <UserButton afterSignOutUrl='/' />
+                    {/* after sign in profile icon to manage user account */}
+                    <MobileNav />
                 </SignedIn>
                 <SignedOut>
                     <Button asChild className='rounded-full' size='lg'>
